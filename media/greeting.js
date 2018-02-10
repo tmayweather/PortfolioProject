db271910a307e29ad7ba71ca800e488a59f41adc
Tnea Mayweather <tnea.mayweather@hotmail.com>
2018-02-09 22:08:21 -0500

    fix nav

--- media/greeting.js
+++ media/greeting.js
@@ -1,3 +1,9 @@
 var name = prompt('Hello! What is your name?');
+var lastName = prompt("What is your last name?");
 var output = document.querySelector('#greeting');
+
+if (name) {
 output.innerHTML = "<p>Thanks for visiting, " + name + ".</p>";
+} else {
+  output.innerHTML = "<p>Please tell us your name!</p>";
+}
