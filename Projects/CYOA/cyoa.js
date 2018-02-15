eaa7978c99b42e81e900b2e66c1ee66a9e61c174
Tnea Mayweather <tnea.mayweather@hotmail.com>
2018-02-15 18:02:56 -0500

    code cleanup

--- Projects/CYOA/cyoa.js
+++ Projects/CYOA/cyoa.js
@@ -1,26 +1,26 @@
 var response = prompt ("Hunter, you have just encountered a huge monster! What do you do? Type 'run', 'fight', or 'hide'. ");
 
-
+// if user's choice is run
 if (response === "run") {
               response = prompt ("You begin to run away. The monster chases you for a while, then starts to slow down. What do you do? Type 'slow down' or 'speed up'");
               if (response === "slow down") {
             response =  alert ("The monster sees that you slowed down and starts picking up speed towards you. He catches up to you and hands you a piece of cake");
-          } else if  (response === "speed up") {
+              } else if  (response === "speed up") {
             response =  alert ("You summon the speed of Sonic The Hedgehog and quickly escape");
               }
         }
 
-
+// if user's choice is hide
 else if (response === "hide") {
 response = prompt ("You hide in a bush and the monster is starting to lose your scent. Choose your next step. Type 'escape' or 'distract'.");
                 if (response === "escape"){
                 response =  alert ("You choose to escape and start to sneak away. As you sneak away the monster spots you and starts to cry. It just wanted to be friends!");
-                }
-                else if (response === "distract") {
+                } else if (response === "distract") {
                 response =  alert ("You throw a rock to distact the monster. When it looks in the other direction, you run away.");
                 }
           }
 
+// if user's choice is fight
 else if (response === "fight") {
 response = prompt ("Choose a weapon. Type 'great hammer', 'sword', or 'foam finger'.");
 
@@ -28,8 +28,7 @@ response = prompt ("Choose a weapon. Type 'great hammer', 'sword', or 'foam fing
               response=  alert ("You pick the sword and slice the monster's leg. It slowly limps away");
                } else if (response === "great hammer") {
               response = alert ("You pick up the mighty great hammer and deal a vicious blow. The monster falls over in pain.");
-              }
-              else if (response === "foam finger") {
+              } else if (response === "foam finger") {
               response = alert ("You pick up the foam finger and the monster laughs as it picks you up with it's mouth and devours you.");
                 }
           }
