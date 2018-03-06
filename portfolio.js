f576e0929b782d5974431a52bb118cc7d01be8d4
Tnea Mayweather <tnea.mayweather@hotmail.com>
2018-03-05 20:09:40 -0500

    redesign. add side nav and about page, change font styles

---
+++ portfolio.js
@@ -0,0 +1,8 @@
+function openMenu(){
+    document.getElementById("sideMenu").style.width ="450px";
+    document.getElementById("pg-content").style.marginLeft = "450px";
+}
+function closeMenu(){
+    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("pg-content").style.marginLeft ="0";
+}
