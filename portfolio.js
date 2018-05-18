3887552273ffac9e62c9b721dbf8806424c3a403
TMayweather <tnea.mayweather@hotmail.com>
2018-05-18 15:59:45 -0400

    work on form validation, cleanup

--- portfolio.js
+++ portfolio.js
@@ -10,3 +10,21 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 
 
 $('.collapse').collapse();
+
+(function () {
+  'use strict';
+  window.addEventListener('load', function () {
+    
+    var forms = document.getElementsByClassName('needs-validation');
+   console.log(forms)
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();  
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
