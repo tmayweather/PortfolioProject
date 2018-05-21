c92e79a7f15df02eb21c8ade39736370a2150feb
tmayweather <tnea.mayweather@hotmail.com>
2018-05-20 21:29:44 -0400

    migrate to react

---
+++ src/js/portfolio.js
@@ -0,0 +1,27 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+$('.collapse').collapse();
+
+(function () {
+  window.addEventListener('load', function () {
+    
+    var forms = document.getElementsByClassName('needs-validation');  
+    var validation = Array.prototype.filter.call(forms, function (form) {
+      form.addEventListener('submit', function (event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();  
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
